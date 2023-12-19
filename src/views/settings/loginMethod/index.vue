7679b212f05c469b098c94ff3eeefa266e6a3317
JunWick <jguller20223@gmail.com>
2023-12-19 19:04:33 +0800

    Add Login Method Admin page.

---
+++ src/views/settings/loginMethod/index.vue
@@ -0,0 +1,231 @@
+<template>
+  <div class="app-container">
+    <!--    button on the table for query-->
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+            v-hasPermi="['settings:loginMethod:add']"
+            icon="Plus"
+            plain
+            size="small"
+            type="primary"
+            @click="handleAdd"
+        >新增
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+            v-hasPermi="['settings:loginMethod:delete']"
+            :disabled="multiple"
+            icon="Delete"
+            plain
+            size="small"
+            type="danger"
+            @click="handleDelete"
+        >删除
+        </el-button>
+      </el-col>
+      <right-toolbar @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <!--    display data in table -->
+    <el-table v-loading="loading" :data="recordList" @selection-change="handleSelectionChange">
+      <el-table-column align="center" type="selection" width="55"/>
+      <el-table-column align="center" label="名字" min-width="180" prop="name"/>
+      <el-table-column align="center" label="代码" min-width="180" prop="code"/>
+      <el-table-column label="地位" prop="status" align="center" width="180">
+        <template #default="scope">
+          <el-switch
+              v-model="scope.row.status"
+              :active-value=1
+              :inactive-value=0
+              @click="toggleSwitch(scope.row)">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" class-name="small-padding fixed-width" fixed="right" label="操作" min-width="150">
+        <template #default="scope">
+          <el-button
+              v-hasPermi="['settings:loginMethod:edit']"
+              icon="Edit" link
+              size="small"
+              type="primary"
+              @click="handleUpdate(scope.row)">修改
+          </el-button>
+          <el-button
+              v-hasPermi="['settings:loginMethod:delete']"
+              icon="Delete" link
+              size="small"
+              style="color: #e05e5e"
+              type="danger"
+              @click="handleDelete(scope.row)">删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+
+    <!-- 添加或修改公司入款银行列表对话框 Add or modify company deposit bank list dialog-->
+    <el-dialog v-model="open" :close-on-click-modal="false" :title="title" append-to-body style="padding-bottom: 20px"
+               width="600px">
+      <el-form ref="queryForm" :model="form" :rules="rules" label-width="100px">
+        <el-form-item label="名字" prop="name">
+          <el-input v-model="form.name" placeholder="名字"/>
+        </el-form-item>
+        <el-form-item label="代码" prop="code" >
+          <el-input v-model="form.code" placeholder="代码"/>
+        </el-form-item>
+        <el-form-item label="地位" prop="status">
+          <el-switch v-model="form.status"
+                     :active-value=1
+                     :inactive-value=0
+          />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="open=false">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup>
+
+import {
+  listRecord,
+  deleteRecord,
+  addRecord,
+  updateRecord,
+  getRecord,
+  changeStatus
+} from "@/api/settings/loginMethod";
+import {getCurrentInstance, reactive, ref, toRefs} from "vue";
+const {proxy} = getCurrentInstance();
+
+const recordList = ref([]);
+const ids = ref([]);
+const title = ref('');
+const loading = ref(true);
+const multiple = ref(true);
+const open = ref(false);
+const data = reactive({
+  /** 查询参数 query params*/
+  queryParams: {},
+
+  /** 表单参数 form parameter*/
+  form: {},
+
+  rules: {
+    name: [
+      {required: true, message: '无效的值', trigger: 'blur'}
+    ],
+    code: [
+      {required: true, message: '无效的值', trigger: 'blur'}
+    ]
+  },
+
+});
+const {queryParams, form, rules} = toRefs(data);
+
+function handleSelectionChange(selection) {
+  ids.value = selection.map(item => item.id);
+  multiple.value = !selection.length;
+}
+
+/** fetch all data from back-end as getList */
+function getList() {
+  loading.value = true;
+  listRecord(queryParams.value).then(response => {
+    recordList.value = response.data;
+    loading.value = false;
+  });
+}
+
+// 表单重置
+function reset() {
+  form.value = {
+    name: null,
+    code: null,
+    status: 1
+  }
+  proxy.resetForm('queryForm');
+}
+
+/** handle add new data */
+function handleAdd() {
+  reset()
+  open.value = true
+  title.value = '添加记录'
+}
+
+/** submit new data and handle insert data api*/
+function submitForm() {
+  proxy.$refs['queryForm'].validate(async valid => {
+    if (valid) {
+      if (form.value.id != null) {
+        updateRecord(form.value).then(() => {
+          proxy.$modal.msgSuccess('修改成功')
+          open.value = false
+          getList()
+        })
+      } else {
+        addRecord(form.value).then(() => {
+          proxy.$modal.msgSuccess('新增成功')
+          open.value = false
+          getList()
+        })
+      }
+    }
+  })
+}
+
+/** handle update data */
+function handleUpdate(row) {
+  getRecord(row.id).then(response => {
+    form.value = response.data;
+  });
+  open.value = true
+  title.value = '更新记录'
+}
+
+/**  删除按钮操作 handle delete */
+function handleDelete(row) {
+  const idss = row.id || ids.value
+  proxy.$confirm('是否确认删除?', '警告', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning'
+  }).then(function () {
+    return deleteRecord(idss)
+  }).then(() => {
+    getList()
+    proxy.$modal.msgSuccess('删除成功')
+  })
+}
+
+
+function toggleSwitch (row) {
+  const text = row.status === 1 ? '启用' : '停用'
+  proxy.$confirm('确认要' + text + '"?', '警告', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消'
+  }).then(function () {
+    let status;
+    loading.value = true
+    status = changeStatus(row.id, row.status);
+    if (status) {
+      return status
+    }
+  }).then(() => {
+    loading.value = false
+    proxy.$modal.msgSuccess(text + '成功')
+    getList()
+  }).catch(function () {
+    loading.value = false
+    row.status = row.status === 0 ? 1 : 0
+  })
+}
+
+getList()
+</script>
