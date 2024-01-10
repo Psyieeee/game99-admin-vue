e86b95a25619fc4b6b15bb8a32dd0b50e21951a3
rajesh <rkyadavsoft@gmail.com>
2024-01-10 13:10:45 +0800

    1

--- src/views/activity/activityManage/activityInfo/index.vue
+++ src/views/activity/activityManage/activityInfo/index.vue
@@ -136,17 +136,17 @@
         </template>
       </el-table-column>
       <el-table-column label="内部跳转类型" align="center" prop="internalJumpType"  min-width="50"/>
-<!--      <el-table-column label="事件跳转状态" align="center" prop="eventJumpStatus"  min-width="100">-->
-<!--        <template #default="scope">-->
-<!--          <el-switch-->
-<!--              v-model="scope.row.eventJumpStatus"-->
-<!--              :active-value="true"-->
-<!--              :inactive-value="false"-->
-<!--              :disabled="true"-->
-<!--              @change="handleEventJumpStatusChange(scope.row)">-->
-<!--          </el-switch>-->
-<!--        </template>-->
-<!--      </el-table-column>-->
+      <el-table-column label="事件跳转状态" align="center" prop="eventJumpStatus"  min-width="100">
+        <template #default="scope">
+          <el-switch
+              v-model="scope.row.eventJumpStatus"
+              :active-value="true"
+              :inactive-value="false"
+              :disabled="true"
+              @change="handleEventJumpStatusChange(scope.row)">
+          </el-switch>
+        </template>
+      </el-table-column>
       <el-table-column label="活动跳跃类型" align="center" prop="eventJumpType"  min-width="50"/>
       <el-table-column label="排序" align="center" prop="sort"  min-width="50"/>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width" fixed="right" min-width="120">
