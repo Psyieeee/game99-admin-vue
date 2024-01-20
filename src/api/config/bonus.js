0812b7fcfe9428bcb6ed5b1870a08f0bf75f92a1
JunWick <jguller20223@gmail.com>
2024-01-20 11:11:23 +0800

    Update config Bonus API reference.

--- src/api/config/bonus.js
+++ src/api/config/bonus.js
@@ -4,7 +4,7 @@ import {url} from "@/utils/url";
 // 查询【请填写功能名称】列表
 export function listConfigBonus(query) {
     return request({
-        url: url.game99PlatformAdminWeb + '/config/bonus/list',
+        url: url.game99PayAdminWeb + '/config/bonus/list',
         method: 'get',
         params: query
     })
@@ -12,7 +12,7 @@ export function listConfigBonus(query) {
 
 export function listConfigBonusPic(query) {
     return request({
-        url: url.game99PlatformAdminWeb + '/config/bonus/listRecommendPic',
+        url: url.game99PayAdminWeb + '/config/bonus/listRecommendPic',
         method: 'get',
         params: query
     })
@@ -21,7 +21,7 @@ export function listConfigBonusPic(query) {
 // 查询【请填写功能名称】详细
 export function getConfigBonus(bonusCode) {
     return request({
-        url: url.game99PlatformAdminWeb + '/config/bonus/' + bonusCode,
+        url: url.game99PayAdminWeb + '/config/bonus/' + bonusCode,
         method: 'get'
     })
 }
@@ -29,7 +29,7 @@ export function getConfigBonus(bonusCode) {
 // 新增【请填写功能名称】
 export function addConfigBonus(data) {
     return request({
-        url: url.game99PlatformAdminWeb + '/config/bonus',
+        url: url.game99PayAdminWeb + '/config/bonus',
         method: 'post',
         data: data
     })
@@ -38,7 +38,7 @@ export function addConfigBonus(data) {
 // 修改【请填写功能名称】
 export function updateConfigBonus(data) {
     return request({
-        url: url.game99PlatformAdminWeb + '/config/bonus',
+        url: url.game99PayAdminWeb + '/config/bonus',
         method: 'put',
         data: data
     })
@@ -47,7 +47,7 @@ export function updateConfigBonus(data) {
 // 修改状态
 export function changeStatus(id, status) {
     return request({
-        url: url.game99PlatformAdminWeb + '/config/bonus/changeStatus/' + id + "/" + status,
+        url: url.game99PayAdminWeb + '/config/bonus/changeStatus/' + id + "/" + status,
         method: 'put'
     })
 }
@@ -55,7 +55,7 @@ export function changeStatus(id, status) {
 // 删除【请填写功能名称】
 export function delConfigBonus(bonusCode) {
     return request({
-        url: url.game99PlatformAdminWeb + '/config/bonus/' + bonusCode,
+        url: url.game99PayAdminWeb + '/config/bonus/' + bonusCode,
         method: 'delete'
     })
 }
