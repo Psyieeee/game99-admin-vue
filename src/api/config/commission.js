736885ec584623578cc61651259e8a60028d4960
jake <jake.feiwin@gmail.com>
2024-02-05 18:56:32 +0800

    Config Commission Admin + redis

---
+++ src/api/config/commission.js
@@ -0,0 +1,61 @@
+import request from '@/utils/request'
+import {url} from "@/utils/url";
+
+// 查询【请填写功能名称】列表
+export function listConfigCommission(query) {
+    return request({
+        url: url.game99PlatformAdminWeb + '/config/commission/list',
+        method: 'get',
+        params: query
+    })
+}
+
+export function listConfigCommissionPic(query) {
+    return request({
+        url: url.game99PlatformAdminWeb + '/config/commission/listRecommendPic',
+        method: 'get',
+        params: query
+    })
+}
+
+// 查询【请填写功能名称】详细
+export function getConfigCommission(bonusCode) {
+    return request({
+        url: url.game99PlatformAdminWeb + '/config/commission/' + bonusCode,
+        method: 'get'
+    })
+}
+
+// 新增【请填写功能名称】
+export function addConfigCommission(data) {
+    return request({
+        url: url.game99PlatformAdminWeb + '/config/commission',
+        method: 'post',
+        data: data
+    })
+}
+
+// 修改【请填写功能名称】
+export function updateConfigCommission(data) {
+    return request({
+        url: url.game99PlatformAdminWeb + '/config/commission',
+        method: 'put',
+        data: data
+    })
+}
+
+// 修改状态
+export function changeStatus(id, status) {
+    return request({
+        url: url.game99PlatformAdminWeb + '/config/commission/changeStatus/' + id + "/" + status,
+        method: 'put'
+    })
+}
+
+// 删除【请填写功能名称】
+export function delConfigCommission(bonusCode) {
+    return request({
+        url: url.game99PlatformAdminWeb + '/config/commission/' + bonusCode,
+        method: 'delete'
+    })
+}
---
+++ src/views/config/commission/index.vue
@@ -0,0 +1,316 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="名称" prop="name">
+        <el-input-number
+            v-model="queryParams.name"
+            placeholder="请输入名称"
+            clearable
+            :step="100"
+            size="small"
+            @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+<!--      <el-form-item label="名称" prop="maxAmount">-->
+<!--        <el-input-number-->
+<!--            v-model="queryParams.maxAmount"-->
+<!--            placeholder="请输入名称"-->
+<!--            clearable-->
+<!--            :step="100"-->
+<!--            size="small"-->
+<!--            @keyup.enter="handleQuery"-->
+<!--        />-->
+<!--      </el-form-item>-->
+      <el-form-item>
+        <el-button type="primary" icon="Search" size="small" @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh" size="small" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+<!--      <el-col :span="1.5">-->
+<!--        <el-button-->
+<!--            type="primary"-->
+<!--            plain-->
+<!--            icon="Plus"-->
+<!--            size="small"-->
+<!--            @click="handleAdd"-->
+<!--            v-hasPermi="['config:commission:add']"-->
+<!--        >新增-->
+<!--        </el-button>-->
+<!--      </el-col>-->
+      <el-col :span="1.5">
+        <el-button
+            type="success"
+            plain
+            icon="Edit"
+            size="small"
+            :disabled="single"
+            @click="handleUpdate"
+            v-hasPermi="['config:commission:edit']"
+        >修改
+        </el-button>
+<!--      </el-col>-->
+<!--      <el-col :span="1.5">-->
+<!--        <el-button-->
+<!--            type="danger"-->
+<!--            plain-->
+<!--            icon="Delete"-->
+<!--            size="small"-->
+<!--            :disabled="multiple"-->
+<!--            @click="handleDelete"-->
+<!--            v-hasPermi="['config:commission:remove']"-->
+<!--        >删除-->
+<!--        </el-button>-->
+      </el-col>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table stripe v-loading="loading" :data="configCommissionList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center"/>
+      <el-table-column label="名字" align="center"    prop="name"/>
+      <el-table-column label="价值" align="center" prop="value"/>
+      <el-table-column align="center" label="状态"     prop="status">
+        <template #default="scope">
+          <el-switch
+              v-model="scope.row.status"
+              :active-value=1
+              :inactive-value=0
+              @change="handleEffect(scope.row)"
+          ></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="更新人" align="center" prop="updatedBy"/>
+      <el-table-column label="更新时间" align="center" prop="updateTime"/>
+      <el-table-column align="center" class-name="small-padding fixed-width" fixed="right" label="操作" width="120">
+        <template #default="scope">
+          <el-button
+              v-hasPermi="['config:commission:edit']"
+              icon="Edit"
+              link
+              size="small"
+              type="primary"
+              @click="handleUpdate(scope.row)"
+          >修改
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+        v-show="total"
+        :total="total"
+        v-model:page="queryParams.pageNum"
+        v-model:limit="queryParams.pageSize"
+        @pagination="getList"
+    />
+
+    <!-- 添加或修改 bonus 配置对话框 Add or modify bonus configuration dialog -->
+    <el-dialog :title="title" v-model="open" width="700px" append-to-body>
+      <el-form ref="commissionRef" :model="form" :rules="rules" label-width="120px" style="padding-bottom: 50px">
+        <el-form-item label="代码" prop="code">
+          <el-input v-model="form.code" placeholder="请输入验证码" disabled/>
+        </el-form-item>
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="form.name" placeholder="请输入姓名"/>
+        </el-form-item>
+        <el-form-item label="价值" prop="value">
+          <el-input v-model="form.value" placeholder="请输入值"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer" style="float: right;margin-top: -20px">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup name="configCommission">
+import {listConfigCommission, getConfigCommission, delConfigCommission, addConfigCommission, updateConfigCommission, changeStatus} from '@/api/config/commission.js'
+import {getCurrentInstance, reactive, ref, toRefs} from "vue";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+const {proxy} = getCurrentInstance();
+
+// 选中数组 ids array
+const ids = ref([]);
+// bonus文件存储服务配置表格数据
+const configCommissionList = ref([]);
+
+//signal data type
+// 弹出层标题
+const title = ref("");
+const total = ref(0);
+const bonusTestTitle = ref("测试图片上传");
+
+
+//boolean types
+// 遮罩层
+const loading = ref(true);
+// 非单个禁用
+const single = ref(true);
+// 非多个禁用
+const multiple = ref(true);
+// 显示搜索条件
+const showSearch = ref(true);
+// 是否显示弹出层
+const open = ref(false);
+
+const data = reactive({
+  // 查询参数
+  queryParams: {
+    pageNum: 1,
+    pageSize: 15,
+    minAmount: null,
+    maxAmount: null,
+    orderByColumn: 'status',
+    isAsc: 'desc'
+  },
+
+  bonusTestForm: {},
+  // 表单参数
+  form: {},
+  // 表单校验
+  rules: {
+    name: [
+      {required: true, message: '需要最小金额', trigger: 'blur'}
+    ],
+    value: [
+      {required: true, message: '最大金额为必填项', trigger: 'blur'}
+    ],
+  }
+});
+const {queryParams, bonusTestForm, form, rules} = toRefs(data);
+
+
+/** 查询bonus服务配置列表 Query the bonus  service configuration list */
+function getList() {
+  loading.value = true
+  listConfigCommission(queryParams.value).then(response => {
+    configCommissionList.value = response.data
+    if (response.total) {
+      total.value = response.total
+    }
+    loading.value = false
+  })
+}
+
+/** 取消按钮 cancel button */
+function cancel() {
+  open.value = false
+  reset()
+}
+
+
+/** 表单重置 Form reset */
+function reset() {
+  form.value = {
+    id: null,
+    minAmount: null,
+    maxAmount: null,
+    bonus: null,
+    multiplier: null,
+    status: 0,
+    updatedBy: null,
+    updateTime: null
+  }
+  proxy.resetForm('commissionRef')
+}
+
+
+/** 搜索按钮操作  Search button action*/
+function handleQuery() {
+  // queryParams.pageNum = 1
+  getList()
+}
+
+
+/** 重置按钮操作 Reset button action*/
+function resetQuery() {
+  proxy.resetForm('queryForm')
+  handleQuery()
+  loading.value =false
+}
+
+
+/**  多选框选中数据 Multi-select box select data */
+function handleSelectionChange(selection) {
+  ids.value = selection.map(item => item.id)
+  single.value = selection.length !== 1
+  multiple.value = !selection.length
+}
+
+/** 新增按钮操作 Add button action*/
+function handleAdd() {
+  reset()
+  open.value = true
+  title.value = '添加佣金配置'
+}
+
+
+/** 修改按钮操作 Modify button action*/
+function handleUpdate(row) {
+  reset()
+  const id = row.id || ids.value
+  getConfigCommission(id).then(response => {
+    form.value = response.data
+    open.value = true
+    title.value = '编辑佣金配置'
+  })
+}
+
+/** 提交按钮 submit button*/
+function submitForm() {
+  proxy.$refs['commissionRef'].validate(valid => {
+    if (valid) {
+      if (form.value.id != null) {
+        updateConfigCommission(form.value).then(() => {
+          proxy.$modal.msgSuccess('修改成功')
+          open.value = false
+          getList()
+        })
+      } else {
+        addConfigCommission(form.value).then(() => {
+          proxy.$modal.msgSuccess('新增成功')
+          open.value = false
+          getList()
+        })
+      }
+    }
+  })
+}
+
+/** 删除按钮操作 delete button action*/
+function handleDelete(row) {
+  const id = row.id || ids.value;
+  proxy.$modal.confirm('您是否确认删除这些数据?', "警告", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning"
+  }).then(function () {
+    return delConfigCommission(id);
+  }).then(() => {
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  })
+}
+
+function handleEffect(row) {
+  proxy.$modal.confirm('确保您要激活的配置配置编号为' + row.id + '"的状态吗?', '警告', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning'
+  }).then(function () {
+    return changeStatus(row.id, row.status)
+  }).then(() => {
+    getList()
+    proxy.$modal.msgSuccess('修改状态成功')
+  }).catch(() => {
+  })
+}
+
+getList()
+
+</script>
