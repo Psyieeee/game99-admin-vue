2bd0eebf36c3a27b0b5e92a8c616ff351fd68190
rajesh <rkyadavsoft@gmail.com>
2024-02-06 11:43:13 +0800

    1

--- src/views/member/memberInfo/index.vue
+++ src/views/member/memberInfo/index.vue
@@ -59,7 +59,7 @@
       <el-form-item prop="hasAccountCharge" style="width: 110px;" id="hasAccountCharge">
         <el-select v-model="queryParams.hasAccountCharge" placeholder="有账户收费" clearable style="width: 110px">
           <el-option value="true" label="是"></el-option>
-          <el-option value="false" label="没有"></el-option>
+          <el-option value="false" label="否"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item prop="version" style="width: 110px;" id="version">
