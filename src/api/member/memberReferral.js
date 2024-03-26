7566147e67d88f1a716f30dc18e2345228af3cc3
jake <jake.feiwin@gmail.com>
2024-03-26 16:51:51 +0800

    change post to get for member referral admin

--- src/api/member/memberReferral.js
+++ src/api/member/memberReferral.js
@@ -4,16 +4,16 @@ import request from "@/utils/request";
 /** 查询列表 list data*/
 export function memberReferralListData(data){
     return request({
-        url:url.game99PlatformAdminWeb+"/member/memberReferral/list/",
-        method:'post',
-        data:data
+        url:url.game99PlatformAdminWeb+"/member/memberReferral/summary/",
+        method:'get',
+        params:data
     })
 }
 
 export function memberReferralReport(data){
     return request({
         url:url.game99PlatformAdminWeb+"/member/memberReferral/listReport/",
-        method:'post',
-        data:data
+        method:'get',
+        params:data
     })
 }
