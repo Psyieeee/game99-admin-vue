7cd26ea82a0213f020bdae2c514f92f399432267
Robert <robertfeiwin@gmail.com>
2024-05-02 14:09:43 +0800

    create admin API for ConfigDepositBonus table to display

---
+++ src/api/activity/configDepositBonus.js
@@ -0,0 +1,53 @@
+import request from "@/utils/request";
+import {url} from "@/utils/url";
+
+
+/**
+ *  activity info list api
+ */
+export function getConfigDepositBonusList(query) {
+    return request({
+        url: url.game99PlatformAdminWeb + "/config/configDepositBonus/list",
+        method: 'get',
+        params: query
+    })
+}
+
+/**
+ *  ADD new config deposit bonus
+ */
+export function addConfigDepositBonus(data) {
+    return request({
+        url: url.game99PlatformAdminWeb + "/config/configDepositBonus/add",
+        method: 'post',
+        data: data
+    })
+}
+
+export function getConfigDepositBonusInfo(id) {
+    return request({
+        url: url.game99PlatformAdminWeb + "/config/configDepositBonus/" + id,
+        method: 'get'
+    })
+}
+
+/**
+ *  UPDATE new config deposit bonus
+ */
+export function updateConfigDepositBonus(data) {
+    return request({
+        url: url.game99PlatformAdminWeb + "/config/configDepositBonus/edit",
+        method: 'put',
+        data: data
+    })
+}
+
+/**
+ *  UPDATE new config deposit bonus
+ */
+export function deleteConfigDepositBonus(ids){
+    return request({
+        url:url.game99PlatformAdminWeb+"/config/configDepositBonus/delete/"+ids,
+        method:'delete'
+    })
+}
